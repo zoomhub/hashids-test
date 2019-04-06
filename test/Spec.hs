d677fced09deb233b54f526dbe9f645114a9f1d5
Daniel Gasienica <daniel@gasienica.ch>
2019-04-05 21:13:19 -0400

    Set up Tasty + Hedgehog

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,43 @@
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Test.Tasty
+import Test.Tasty.ExpectedFailure
+import Test.Tasty.Hedgehog
+
+genAlphaList :: Gen String
+genAlphaList =
+  Gen.list (Range.linear 0 100) Gen.alphaNum
+
+test_involutive :: (MonadTest m, Eq a, Show a) => (a -> a) -> a -> m ()
+test_involutive f x =
+  f (f x) === x
+
+prop_reverse_involutive :: Property
+prop_reverse_involutive =
+  property $ do
+    xs <- forAll genAlphaList
+    test_involutive reverse xs
+
+badReverse :: [a] -> [a]
+badReverse [] = []
+badReverse [_] = []
+badReverse (x : xs) = badReverse xs ++ [x]
+
+prop_badReverse_involutive :: Property
+prop_badReverse_involutive =
+  property $ do
+    xs <- forAll genAlphaList
+    test_involutive badReverse xs
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  defaultMain $
+  testGroup "hashids.sql vs hashids.js"
+    [ testProperty
+        "reverse involutive"
+        prop_reverse_involutive
+    , expectFail $ testProperty
+        "badReverse involutive fails"
+         prop_badReverse_involutive
+    ]
